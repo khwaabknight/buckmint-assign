5e87e37e6db0db58fd5f4b06f801b902bf413532
Aman Gupta <khwaabknight@gmail.com>
2024-03-12 17:45:02 +0530

    navbar overlap fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ function App() {
 
   return (
     <div className="grid lg:grid-cols-6 sm:grid-cols-12 lg:gap-20 relative">
-      <div className={`lg:w-1/6 fixed z-20 sm:block min-h-screen transition-[width] duration-300 ${sidebarOn ? 'max-w-80 w-4/5 sm:w-3/4' : 'w-0 sm:w-1/12'} `}>
+      <div className={`lg:w-1/6 fixed sm:block min-h-screen transition-[width] duration-300 ${sidebarOn ? 'max-w-80 w-4/5 sm:w-3/4 z-50' : 'w-0 sm:w-1/12'} `}>
         <Sidebar/>
       </div>
       <div className='lg:col-span-1 sm:col-span-1'/>
       <div className='lg:col-span-5 sm:col-span-11 w-full relative'>
-        <div className='fixed top-0 right-0 bg-white lg:w-5/6 sm:w-11/12 w-full sm:min-h-24 min-h-10 z-50'>
+        <div className='fixed top-0 right-0 bg-white lg:w-5/6 sm:w-11/12 w-full sm:min-h-24 min-h-10 z-40'>
           <Navbar/>
         </div>
         <div className='sm:min-h-24 min-h-10'/>
